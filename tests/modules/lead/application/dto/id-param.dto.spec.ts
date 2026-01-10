704ae7d07d691bd75df18c91dc13ab28003e61f4
Robson Maia <robsonmvieira@gmail.com>
2026-01-10 16:39:07 +0000

    fix: prettier formatting on id-param.dto.spec.ts

--- tests/modules/lead/application/dto/id-param.dto.spec.ts
+++ tests/modules/lead/application/dto/id-param.dto.spec.ts
@@ -1,7 +1,4 @@
-import {
-  IdParamDto,
-  IdParamDtoValidator
-} from '@modules/lead/application/dtos'
+import { IdParamDto, IdParamDtoValidator } from '@modules/lead/application/dtos'
 
 describe('IdParamDto', () => {
   const validUUID = '550e8400-e29b-41d4-a716-446655440000'
