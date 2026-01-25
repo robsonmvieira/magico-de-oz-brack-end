fb9a38b0f77f94167fe1d085ea92444394067c06
Robson Maia <robsonmvieira@gmail.com>
2026-01-25 11:26:02 +0000

    feat(lead): integrate country module in lead module
    - Register CountryController in controllers
    - Register CountryImportProcessor for Bull queue
    - Register COUNTRY_IMPORT_QUEUE
    - Add COUNTRY_PROVIDERS use cases

--- src/modules/lead/lead.module.ts
+++ src/modules/lead/lead.module.ts
@@ -5,12 +5,14 @@ import {
   LeadCategoryController,
   LeadController,
   SimpleController,
-  PartnerController
+  PartnerController,
+  CountryController
 } from './application/controllers'
 import {
   LEAD_CATEGORY_PROVIDERS,
   LEAD_PROVIDERS,
-  PARTNER_PROVIDERS
+  PARTNER_PROVIDERS,
+  COUNTRY_PROVIDERS
 } from './infra/providers'
 import { SharedModule } from '@modules/shared/shared.module'
 import { SIMPLE_PROVIDERS } from './infra/providers/simple.provider'
@@ -18,7 +20,9 @@ import {
   SimpleImportProcessor,
   SIMPLE_IMPORT_QUEUE,
   PartnerImportProcessor,
-  PARTNER_IMPORT_QUEUE
+  PARTNER_IMPORT_QUEUE,
+  CountryImportProcessor,
+  COUNTRY_IMPORT_QUEUE
 } from './infra/queues'
 import { EnvModule, EnvService } from '@modules/env'
 
@@ -39,26 +43,30 @@ import { EnvModule, EnvService } from '@modules/env'
     }),
     BullModule.registerQueue(
       { name: SIMPLE_IMPORT_QUEUE },
-      { name: PARTNER_IMPORT_QUEUE }
+      { name: PARTNER_IMPORT_QUEUE },
+      { name: COUNTRY_IMPORT_QUEUE }
     )
   ],
   controllers: [
     LeadController,
     LeadCategoryController,
     SimpleController,
-    PartnerController
+    PartnerController,
+    CountryController
   ],
   providers: [
     LeadService,
     SimpleImportProcessor,
     PartnerImportProcessor,
+    CountryImportProcessor,
     ...Object.values(LEAD_CATEGORY_PROVIDERS.REPOSITORY_PROVIDERS),
     ...Object.values(LEAD_CATEGORY_PROVIDERS.USE_CASES_PROVIDERS),
     ...Object.values(LEAD_PROVIDERS.REPOSITORY_PROVIDERS),
     ...Object.values(LEAD_PROVIDERS.USE_CASES_PROVIDERS),
     ...Object.values(SIMPLE_PROVIDERS.SERVICE_PROVIDERS),
     ...Object.values(SIMPLE_PROVIDERS.USE_CASES_PROVIDERS),
-    ...Object.values(PARTNER_PROVIDERS.USE_CASES_PROVIDERS)
+    ...Object.values(PARTNER_PROVIDERS.USE_CASES_PROVIDERS),
+    ...Object.values(COUNTRY_PROVIDERS.USE_CASES_PROVIDERS)
   ]
 })
 export class LeadModule {}
